0d51f0aeda723d168e757786235910c019a34d84
KarimHweidy97 <134971286+KarimHweidy97@users.noreply.github.com>
2023-09-11 22:09:04 +0300

    adding  library layer for project

---
+++ liberary/ErrorType.h
@@ -0,0 +1,16 @@
+#ifndef ERRORTYPE_H
+#define ERRORTYPE_H
+
+
+
+#define NULL              ((void*)0)
+
+
+#define OK                            1u
+#define NOK                           2u
+#define NULL_PTR_ERR                  3u
+
+
+
+
+#endif
